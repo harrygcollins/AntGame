0282376d03a3bab7daaf535f5249d29554fd50c6
JoshCollet <Joshcollet@hotmail.co.uk>
2016-04-21 11:58:51 +0100

    improvements 1.0

--- src/antgame/Chemistry.java
+++ src/antgame/Chemistry.java
@@ -9,45 +9,72 @@ package antgame;
  * @author jc573
  */
 public class Chemistry {
-Ant a;
+
     public enum markerType {
 
         BASE, WARNING, FriendWithFood, FOOD, EDGE, DIED;
     }
 
-    public int placeMarker(markerType m) throws Exception {
-        switch (m) {
-            case BASE:
-                return 0;
-            case WARNING:
-                return 1;
-            case FriendWithFood:
-                return 2;
-            case FOOD:
-                return 3;
-            case EDGE:
-                return 4;
-            case DIED:
-                return 5;
-            default:
-                throw new Exception("Invaid Case");
+    class markers {
+
+        markers(markerType m, int colour) {
+            int team;
+            markerType type;
+           
+            team = colour;
+            type = m;
+
 
         }
-        
+    }
 
+        public int ChooseMarker(markerType m) throws Exception {
+
+            switch (m) {
+                case BASE:
+                    return 0;
+
+                case WARNING:
+                    return 1;
+
+                case FriendWithFood:
+                    return 2;
+
+                case FOOD:
+                    return 3;
+
+                case EDGE:
+                    return 4;
+
+                case DIED:
+                    return 5;
+
+                default:
+                    throw new Exception("Invaid Marker");
+
+            }
+        }
+
+        public void placeMarker(Ant a){
+            int x, y;
+            x = a.positionX;
+            y = a.positionY;
+            
+            
+                    
+        //Check cell to see if it is available
+            // if its available:
+                    // create a marker object with the marker type and colour
+                    // place marker on board at given co-ordinates
+           // if the cell is taken run clearMarker method
+       
 
     }
 
-    public void clearMarker() {
+public void clearMarker() {
 //        1.Sense Cell
 //        2.if cell contains a marker object from other team marker remove the object
-//        3.markers are checked for the colour of origin which is stored in the marker
-    }
-    class marker{
-        
-       
-        marker(markerType m, int x , int y, int c){
-            
+//        3.markers are checked for the colour of origin o prevent removal of own teams markers which is stored in the marker
         }
     }
-}
+
