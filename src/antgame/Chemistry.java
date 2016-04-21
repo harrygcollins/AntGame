fae96ff92a3e07e1b62294207f34c982a56aee51
JoshCollet <Joshcollet@hotmail.co.uk>
2016-04-21 12:22:45 +0100

    Chemistry 1.1
    Foundations for chemistry in place

--- src/antgame/Chemistry.java
+++ src/antgame/Chemistry.java
@@ -10,71 +10,86 @@ package antgame;
  */
 public class Chemistry {
 
+    public int typeChoice = 0;
+
     public enum markerType {
 
         BASE, WARNING, FriendWithFood, FOOD, EDGE, DIED;
+  //    0     1        2               3      4     5
     }
 
-    class markers {
+   
 
-        markers(markerType m, int colour) {
-            int team;
-            markerType type;
-           
+    public int ChooseMarker(markerType m) throws Exception {
+
+        switch (m) {
+            case BASE:
+                return 0;
+
+            case WARNING:
+                return 1;
+
+            case FriendWithFood:
+                return 2;
+
+            case FOOD:
+                return 3;
+
+            case EDGE:
+                return 4;
+
+            case DIED:
+                return 5;
+
+            default:
+                throw new Exception("Invaid Marker");
+
+        }
+    }
+
+    public void placeMarker(Ant a, World w) {
+        int x, y;
+        boolean taken;
+        taken = false;
+        x = a.positionX;
+        y = a.positionY;
+        // Please Clarrify or add
+        // This method must now check the cell at the ants location to ensure no marker is placed
+        // if sensed cell has a marker check marker.team to see if it belongs to the enemy if it does  
+        // run remove method
+
+//        if (taken) {
+//            clearMarker(x, y);
+//        }
+
+
+        //  Else create marker, value must be that returned from Choosing Type Method
+        typeChoice = ChooseMarker(markerType);
+
+        markers marker;
+        marker = new markers(typeChoice, a.colour);
+        //place object on world
+        
+        
+  
+    }
+    
+    
+    
+    
+    
+    // markers object to be placed on board
+    public class markers {
+        public int team;
+        public int type;
+
+        markers(int markerType, int colour) {
             team = colour;
-            type = m;
-
-
+            type = markerType;
         }
     }
 
-        public int ChooseMarker(markerType m) throws Exception {
-
-            switch (m) {
-                case BASE:
-                    return 0;
-
-                case WARNING:
-                    return 1;
-
-                case FriendWithFood:
-                    return 2;
-
-                case FOOD:
-                    return 3;
-
-                case EDGE:
-                    return 4;
-
-                case DIED:
-                    return 5;
-
-                default:
-                    throw new Exception("Invaid Marker");
-
-            }
-        }
-
-        public void placeMarker(Ant a){
-            int x, y;
-            x = a.positionX;
-            y = a.positionY;
-            
-            
-                    
-        //Check cell to see if it is available
-            // if its available:
-                    // create a marker object with the marker type and colour
-                    // place marker on board at given co-ordinates
-           // if the cell is taken run clearMarker method
-       
-
+    public void clearMarker(int x, int y) {
+      //        remove enemy marker from world
     }
-
-public void clearMarker() {
-//        1.Sense Cell
-//        2.if cell contains a marker object from other team marker remove the object
-//        3.markers are checked for the colour of origin o prevent removal of own teams markers which is stored in the marker
-        }
-    }
-
+}
