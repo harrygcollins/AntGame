e9ff62f566d53c622902d390a076b7482d6215ef
digenis16 <ad485@sussex.ac.uk>
2016-05-03 20:32:36 +0100

    Comments added

--- src/antgame/MainMenu.java
+++ src/antgame/MainMenu.java
@@ -19,16 +19,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 /**
- *
+ * Constructs the UI for main menu of the game
+ * to choose between tournament and single match
+ * 
  * @author ADigenis
  */
 public class MainMenu extends JFrame {
 
+    /**
+     * Constructor of the single match. Builds the UI for main menu
+     */
     public MainMenu() {
         super("Ant-Game");
         buildUI();
     }
-
+    
+    /**
+     * Creates the UI for the main menu
+     */
     private void buildUI() {
         Container c;
         JLabel instruction;
@@ -40,31 +48,60 @@ public class MainMenu extends JFrame {
         singleMatch = new JButton("Single Match");
         tournament = new JButton("Tournament");
 
+        //adds rigid area to the frame
         c.add(Box.createRigidArea(new Dimension(0, 80)));
         c.add(instruction);
+        
         instruction.setAlignmentX(CENTER_ALIGNMENT);
         instruction.setFont(new Font("Ravie", Font.PLAIN, 80));
+        
+        //adds rigid area to the frame
         c.add(Box.createRigidArea(new Dimension(0, 200)));
         c.add(singleMatch);
+        
+        // change format of the button
         singleMatch.setAlignmentX(CENTER_ALIGNMENT);
         singleMatch.setBorderPainted(false);
         singleMatch.setFocusPainted(false);
         singleMatch.setContentAreaFilled(false);
         singleMatch.setFont(new Font("Times New Roman", Font.PLAIN, 60));
+        
+        /**
+         * creates effect when hovering over the button
+         */
         singleMatch.addMouseListener(new MouseAdapter() {
             Font f = singleMatch.getFont();
 
+            /**
+             * makes the text of the button bold when mouse enters the area of
+             * the button
+             *
+             * @param me the event that mouse entered button area
+             */
             @Override
             public void mouseEntered(MouseEvent me) {
                 singleMatch.setFont(new Font("Times New Roman", Font.BOLD, 60));
             }
 
+            /**
+             * sets the font of the button as it was before entering the area
+             * button
+             *
+             * @param me the event that mouse left button area
+             */
             @Override
             public void mouseExited(MouseEvent me) {
                 singleMatch.setFont(f);
             }
         });
+        
         singleMatch.addActionListener(new ActionListener() {
+            /**
+             * handles the event when the button is clicked. Close the main menu
+             * frame and creates a single match frame
+             *
+             * @param e the event the table has been clicked.
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
@@ -73,27 +110,52 @@ public class MainMenu extends JFrame {
             }
         });
 
+        //adds rigid area to the frame
         c.add(Box.createRigidArea(new Dimension(0, 100)));
         c.add(tournament);
+        
+        // change format of the button
         tournament.setAlignmentX(CENTER_ALIGNMENT);
         tournament.setBorderPainted(false);
         tournament.setFocusPainted(false);
         tournament.setContentAreaFilled(false);
         tournament.setFont(new Font("Times New Roman", Font.PLAIN, 60));
+        
+        /**
+         * creates effect when hovering over the button
+         */
         tournament.addMouseListener(new MouseAdapter() {
             Font f = tournament.getFont();
 
+            /**
+             * makes the text of the button bold when mouse enters the area of
+             * the button
+             *
+             * @param me the event that mouse entered button area
+             *
             @Override
             public void mouseEntered(MouseEvent me) {
                 tournament.setFont(new Font("Times New Roman", Font.BOLD, 60));
             }
-
+            
+            /**
+             * sets the font of the button as it was before entering the area
+             * button
+             *
+             * @param me the event that mouse left button area
+             */
             @Override
             public void mouseExited(MouseEvent me) {
                 tournament.setFont(f);
             }
         });
         tournament.addActionListener(new ActionListener() {
+            /**
+             * handles the event when the button is clicked. Close the main menu
+             * frame and creates a tournament frame
+             *
+             * @param e the event the table has been clicked.
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
--- src/antgame/SingleMatch.java
+++ src/antgame/SingleMatch.java
@@ -5,7 +5,6 @@
  */
 package antgame;
 
-//import static java.accessibility.util.AWTEventMonitor.addActionListener;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -31,6 +30,7 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 /**
+ * Constructs the UI for a single match game
  *
  * @author ADigenis
  */
@@ -42,11 +42,17 @@ public class SingleMatch extends JFrame {
     private PlayGame newGame;
     private int winner;
 
+    /**
+     * Constructor of the single match. Builds the UI for single match
+     */
     public SingleMatch() {
         super("Single Match");
         buildUI();
     }
 
+    /**
+     * Adds users buttons and the world field the container
+     */
     private void buildUI() {
         Container c;
         c = getContentPane();
@@ -62,6 +68,12 @@ public class SingleMatch extends JFrame {
         setVisible(true);
     }
 
+    /**
+     * Creates the panel for the users which includes buttons to upload
+     * ant-brains and a button to start the game and returns it.
+     *
+     * @return JPanel including users button and start game button
+     */
     private JPanel createUsersInfoAndStart() {
         JPanel outerPanel, userInfo;
         JLabel team1, team2;
@@ -69,39 +81,59 @@ public class SingleMatch extends JFrame {
 
         userInfo = new JPanel();
         outerPanel = new JPanel();
-        team1 = new JLabel("Team 1");
+        team1 = new JLabel("Red Team");
         team1.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        team2 = new JLabel("Team 2");
+        team2 = new JLabel("Black Team");
         team2.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 
         startBtn = new JButton("Start Game");
         startBtn.setBorderPainted(false);
         startBtn.setFocusPainted(false);
         startBtn.setContentAreaFilled(false);
-        startBtn.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        startBtn.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+        /**
+         * creates effect when hovering over the button
+         */
         startBtn.addMouseListener(new MouseAdapter() {
             Font f = startBtn.getFont();
 
+            /**
+             * makes the text of the button bold when mouse enters the area of
+             * the button
+             *
+             * @param me the event that mouse entered button area
+             */
             @Override
             public void mouseEntered(MouseEvent me) {
-                startBtn.setFont(new Font("Times New Roman", Font.BOLD, 16));
+                startBtn.setFont(new Font("Times New Roman", Font.BOLD, 24));
             }
 
+            /**
+             * sets the font of the button as it was before entering the area
+             * button
+             *
+             * @param me the event that mouse left button area
+             */
             @Override
             public void mouseExited(MouseEvent me) {
                 startBtn.setFont(f);
             }
         });
         startBtn.addActionListener(new ActionListener() {
+            /**
+             * handles the event when the button is clicked.
+             *
+             * @param e the event the table has been clicked.
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (antBrainTeam1 != null && antBrainTeam2 != null && newWorld != null) {
                     newGame = new PlayGame(antBrainTeam1, antBrainTeam2, newWorld);
                     winner = newGame.runGame();
                     if (winner == 0) {
-                        JOptionPane.showMessageDialog(rootPane, "The winner is Team 1!");
+                        JOptionPane.showMessageDialog(rootPane, "Red team wins!");
                     } else if (winner == 1) {
-                        JOptionPane.showMessageDialog(rootPane, "The winner is Team 2!");
+                        JOptionPane.showMessageDialog(rootPane, "Black team wins!");
                     } else {
                         JOptionPane.showMessageDialog(rootPane, "Draw!");
                     }
@@ -116,21 +148,42 @@ public class SingleMatch extends JFrame {
         uploadTeam1.setBorderPainted(false);
         uploadTeam1.setFocusPainted(false);
         uploadTeam1.setContentAreaFilled(false);
-        uploadTeam1.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        uploadTeam1.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        /**
+         * creates effect when hovering button
+         */
         uploadTeam1.addMouseListener(new MouseAdapter() {
             Font f = uploadTeam1.getFont();
 
+            /**
+             * makes the text of the button bold when mouse enters the area of
+             * the button
+             *
+             * @param me the event that mouse entered button area
+             */
             @Override
             public void mouseEntered(MouseEvent me) {
-                uploadTeam1.setFont(new Font("Times New Roman", Font.BOLD, 16));
+                uploadTeam1.setFont(new Font("Times New Roman", Font.BOLD, 18));
             }
 
+            /**
+             * sets the font of the button as it was before entering the area
+             * button
+             *
+             * @param me the event that mouse left button area
+             */
             @Override
             public void mouseExited(MouseEvent me) {
                 uploadTeam1.setFont(f);
             }
         });
         uploadTeam1.addActionListener(new ActionListener() {
+            /**
+             * handles the event when the button is clicked. Opens a file
+             * chooser window and adds text of the file into array list.
+             *
+             * @param e the event the table has been clicked.
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser();
@@ -147,21 +200,42 @@ public class SingleMatch extends JFrame {
         uploadTeam2.setBorderPainted(false);
         uploadTeam2.setFocusPainted(false);
         uploadTeam2.setContentAreaFilled(false);
-        uploadTeam2.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        uploadTeam2.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        /**
+         * creates effect when hovering button
+         */
         uploadTeam2.addMouseListener(new MouseAdapter() {
             Font f = uploadTeam2.getFont();
 
+            /**
+             * makes the text of the button bold when mouse enters the area of
+             * the button
+             *
+             * @param me the event that mouse entered button area
+             */
             @Override
             public void mouseEntered(MouseEvent me) {
-                uploadTeam2.setFont(new Font("Times New Roman", Font.BOLD, 16));
+                uploadTeam2.setFont(new Font("Times New Roman", Font.BOLD, 18));
             }
 
+            /**
+             * sets the font of the button as it was before entering the area
+             * button
+             *
+             * @param me the event that mouse left button area
+             */
             @Override
             public void mouseExited(MouseEvent me) {
                 uploadTeam2.setFont(f);
             }
         });
         uploadTeam2.addActionListener(new ActionListener() {
+            /**
+             * handles the event when the button is clicked. Opens a file
+             * chooser window and adds text of the file into array list.
+             *
+             * @param e the event the table has been clicked.
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser();
@@ -188,11 +262,16 @@ public class SingleMatch extends JFrame {
 
         outerPanel.add(userInfo, BorderLayout.NORTH);
         outerPanel.add(startBtn, BorderLayout.SOUTH);
-        outerPanel.setPreferredSize(new Dimension(150, 0));
+        outerPanel.setPreferredSize(new Dimension(250, 0));
 
         return outerPanel;
     }
 
+    /**
+     * Creates a panel with a jtextarea with world printed
+     *
+     * @return JPanel including a JTextArea
+     */
     public JPanel createWorldField() {
         JTextArea worldField;
         JPanel outerPanel;
@@ -200,7 +279,9 @@ public class SingleMatch extends JFrame {
         outerPanel = new JPanel();
         outerPanel.setLayout(new BoxLayout(outerPanel, BoxLayout.LINE_AXIS));
         outerPanel.setBorder(new TitledBorder(new LineBorder(Color.GRAY, 1), "Ant-World"));
+        //prints the world into the text area
         worldField = new JTextArea(newWorld.testWorld());
+        //set font to fix spacing
         worldField.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
         outerPanel.add(worldField);
 
