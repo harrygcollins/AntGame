135b0730c435a10c9563364c48fec6ab689cb904
harrygcollins <Harry@MacBook-Pro-2.local>
2016-05-04 03:11:44 +0100

    Enum Java Docs
    Added java docs for the Enum classes.

--- src/antgame/markerType.java
+++ src/antgame/markerType.java
@@ -11,6 +11,34 @@ package antgame;
  */
 public enum markerType {
 
-        BASE, WARNING, FriendWithFood, FOOD, EDGE, DIED;
-  //    0     1        2               3      4     5
+    /**
+     * Enum representing marker 0, base.
+     */
+    BASE,
+
+    /**
+     * Enum representing marker 1, warning.
+     */
+    WARNING,
+
+    /**
+     * Enum representing marker 2, FriendWithFood.
+     */
+    FriendWithFood,
+
+    /**
+     * Enum representing marker 3, food..
+     */
+    FOOD,
+
+    /**
+     * Enum representing marker 4, edge.
+     */
+    EDGE,
+
+    /**
+     * Enum representing marker 5, died.
+     */
+    DIED;
+
     }
--- src/antgame/senseDir.java
+++ src/antgame/senseDir.java
@@ -10,5 +10,24 @@ package antgame;
  * @author Harry
  */
 public enum senseDir {
-        HERE, AHEAD, LEFTAHEAD, RIGHTAHEAD;
+
+    /**
+     *  Enum representing direction here
+     */
+    HERE,
+
+    /**
+     *  Enum representing direction ahead
+     */
+    AHEAD,
+
+    /**
+     *  Enum representing direction left-ahead
+     */
+    LEFTAHEAD,
+
+    /**
+     *  Enum representing direction right-ahead
+     */
+    RIGHTAHEAD;
 }
